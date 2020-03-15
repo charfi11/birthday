d215e3635963e84823e30e1866b2cf1bf6739ca2
charfi11 <m.charfi232@laposte.net>
2020-03-15 17:04:39 +0100

    first

---
+++ public/app.js
@@ -0,0 +1,147 @@
+const ul = document.getElementById('list');
+const fb = document.getElementById('formBirthday');
+const fbdiv = document.getElementById('fbirth')
+const form = document.getElementById('formUpdate');
+const birth = document.getElementById('birth');
+const upd = document.getElementById('updatefr');
+const closes = document.getElementById('close');
+const closesup = document.getElementById('close2');
+const divM = document.getElementById('divM');
+
+closes.addEventListener('click', (event) => {
+    fbdiv.style.display = 'none';
+    birth.style.display = 'block';
+});
+
+closesup.addEventListener('click', (event) => {
+    form.style.display = 'none';
+    birth.style.display = 'block';
+});
+
+//function for create element
+function renderBirthday(childSnapshot) {
+    var li = document.createElement('li');
+    var div = document.createElement('div');
+    var del = document.createElement('div');
+    var up = document.createElement('div');
+    var pname = document.createElement('p');
+    var pdate = document.createElement('p');
+    div.setAttribute('class', 'blockcrud');
+    del.setAttribute('class', 'delete');
+    up.setAttribute('class', 'update');
+
+    li.setAttribute('data-id', childSnapshot.id);
+    pname.textContent = childSnapshot.data().name;
+    pdate.textContent = childSnapshot.data().date;
+
+    del.innerHTML = "<i class='fas fa-trash-alt' data-toggle='tooltip' data-placement='top' title='cliquez pour supprimer un anniversaire'></i>";
+    up.innerHTML = '<i class="fas fa-edit" data-toggle"tooltip" data-placement="top" title="cliquez pour modifier un anniversaire"></i>';
+
+    li.appendChild(pname);
+    li.appendChild(pdate);
+    li.appendChild(div);
+    div.appendChild(del);
+    div.appendChild(up);
+    ul.appendChild(li);
+
+    //event to delete data
+    del.addEventListener("click", (event) => {
+        event.stopPropagation();
+        var id = event.target.parentElement.parentElement.parentElement.getAttribute('data-id');
+        db.collection('one').doc(id).delete();
+        event.target.parentElement.parentElement.parentElement.remove();
+    });
+
+    //event to replace data in form
+    up.addEventListener('click', (event) => {
+        event.stopPropagation();
+        var id = event.target.parentElement.parentElement.parentElement.getAttribute('data-id');
+        var parent = event.target.parentElement.parentElement.parentElement;
+        var input = document.getElementById('hid1');
+        fbdiv.style.display = 'none';
+        birth.style.display = 'block';
+        upd.style.display = 'block';
+        input.setAttribute('data-id', id);
+        form.name.value = parent.children[0].textContent;
+        form.date.value = parent.children[1].textContent;
+    });
+
+    //form for update data
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        var id = document.getElementById('hid1').getAttribute('data-id');
+        
+        var name = form.name.value;
+        var date = form.date.value;
+        var data = {
+            name: name,
+            date: date
+        }
+        db.collection('one').doc(id).update(data);
+        var f = document.getElementById('updatefr');
+        f.style.display = 'none';
+    });
+}
+
+birth.addEventListener('click', (event) => {
+    fbdiv.style.display = 'flex';
+    upd.style.display = 'none';
+    birth.style.display = 'none';
+});
+
+// form for send data
+fb.addEventListener("submit", (event) => {
+    event.preventDefault();
+    var id = Math.random();
+    var data = {
+        name: fb.name.value,
+        date: fb.date.value,
+        userid: fb.userid.value,
+        id: id,
+    };
+    divM.style.display = 'none';
+    db.collection('one').add(data);
+    fbdiv.style.display = 'none';
+    birth.style.display = 'block';
+    var li = document.createElement('li');
+    var div = document.createElement('div');
+    var del = document.createElement('div');
+    var up = document.createElement('div');
+    var pname = document.createElement('p');
+    var pdate = document.createElement('p');
+    div.setAttribute('class', 'blockcrud');
+    del.setAttribute('class', 'delete');
+    up.setAttribute('class', 'update');
+
+    li.setAttribute('data-id', data.id);
+    pname.textContent = data.name;
+    pdate.textContent = data.date;
+
+    del.innerHTML = "<i class='fas fa-trash-alt'></i>";
+    up.innerHTML = '<i class="fas fa-edit"></i>';
+
+    li.appendChild(pname);
+    li.appendChild(pdate);
+    li.appendChild(div);
+    div.appendChild(del);
+    div.appendChild(up);
+    ul.appendChild(li);
+    fb.name.value = '';
+    fb.date.value = '';
+
+    //event to replace data in form
+    up.addEventListener('click', (event) => {
+        event.stopPropagation();
+        var id = event.target.parentElement.parentElement.parentElement.getAttribute('data-id');
+        var parent = event.target.parentElement.parentElement.parentElement;
+        var input = document.getElementById('hid1');
+        fbdiv.style.display = 'none';
+        birth.style.display = 'block';
+        form.style.display = 'block';
+        upd.style.display = 'block';
+        input.setAttribute('data-id', id);
+        form.name.value = parent.children[0].textContent;
+        form.date.value = parent.children[1].textContent;
+        form.id.value = input.getAttribute('data-id');
+    });
+});
---
+++ public/home.js
@@ -0,0 +1,110 @@
+const nav = document.getElementById('blocknav');
+nav.style.display = 'none';
+
+//trace user status changed
+auth.onAuthStateChanged(user => {
+    if (user) {
+        if (user != null) {
+            document.getElementById('profilmail').innerHTML = user.email;
+        };
+        const blockB = document.getElementById('blockB');
+        const blockH = document.getElementById('blockH');
+        const fb = document.getElementById('formBirthday');
+        const nav = document.getElementById('blocknav');
+        const uid = user.uid;
+        const inputid = document.createElement('input');
+        inputid.setAttribute('type', 'hidden');
+        inputid.setAttribute('value', uid);
+        inputid.setAttribute('name', 'userid');
+        inputid.setAttribute('id', 'uid');
+        fb.appendChild(inputid);
+
+        blockH.style.display = 'none';
+        blockB.style.display = 'block';
+        nav.style.display = 'block';
+
+        //loop for read data
+        db.collection('one').where('userid', '==', user.uid).orderBy('name').get().then((snapshot => {
+            if (snapshot.docs.length >= 1) {
+                const dm = document.getElementById('divMore');
+                if (dm != null) {
+                    dm.remove();
+                }
+                snapshot.docs.forEach(childSnapshot => {
+                    renderBirthday(childSnapshot);
+                });
+            } else {
+                const createDm = document.createElement('div');
+                createDm.setAttribute('id', 'divMore');
+                divM.appendChild(createDm);
+                const dm = document.getElementById('divMore');
+                dm.innerHTML = "<h5 class='message'>Ajouter un anniversaire en cliquant sur le gâteau en haut.</h5><i class='fas fa-arrow-up ml-2'></i>";
+            }
+        }));
+    } else {
+        blockH.style.display = 'block';
+        blockB.style.display = 'none';
+        nav.style.display = 'none';
+    }
+});
+
+const signupForm = document.getElementById('signupForm');
+
+//signup user
+signupForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const mail = signupForm['mail'].value;
+    const password = signupForm['password'].value;
+    auth.createUserWithEmailAndPassword(mail, password).then(cred => {
+            $('#exampleModalCenter').modal('hide');
+            signupForm.reset();
+        })
+        .catch((error) => {
+            var errorMessage = error.message;
+            console.log();
+            signupForm.querySelector('.error').textContent = errorMessage;
+        });
+});
+
+//logout
+const logout = document.getElementById('logout');
+logout.addEventListener('click', (event) => {
+    event.preventDefault();
+    const ul = document.getElementById('list').children;
+
+    auth.signOut().then(() => {
+        const dm = document.getElementById('divMore');
+        if (dm != null) {
+            dm.remove();
+        }
+        const arr = Array.from(ul);
+        arr.forEach(element => element.remove());
+    });
+    var id = document.getElementById('uid');
+    id.remove();
+});
+
+//login
+const loginForm = document.getElementById('loginForm');
+loginForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const mail = loginForm['mailLogin'].value;
+    const password = loginForm['passwordLogin'].value;
+
+    auth.signInWithEmailAndPassword(mail, password).then(cred => {
+            $('#exampleModalCenter1').modal('hide');
+            loginForm.reset();
+        })
+        .catch((error) => {
+            var errorMessage = error.message;
+            loginForm.querySelector('.error').textContent = errorMessage;
+        });
+});
+
+var userp = document.getElementById('userp');
+
+userp.addEventListener('click', (event) => {
+ var profil = document.getElementById('profile');
+
+ profil.style.display = 'block';
+});
